710af9e357239ac827eb758380edd937f3220ca9
TheCookingUnderground <147450385+TheCookingUnderground@users.noreply.github.com>
2024-07-17 20:58:50 -0400

    Add project files.

---
+++ Penguins/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Penguins.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
